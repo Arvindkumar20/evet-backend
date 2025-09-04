3fc6ec0c84101ac5fa346129bfc3c50cb53227ee
arvindkumar20 <arvindkumar8960905167@gmail.com>
2025-09-05 01:55:20 +0530

    complete full stack mini event tracker app

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -7,41 +7,41 @@ import { env } from '../config/env.js';
 const router = Router();
 
 function setAuthCookie(res, token) {
-  res.cookie('token', token, {
-    httpOnly: true,
-    sameSite: 'lax',
-    secure: env.COOKIE_SECURE,
-    maxAge: 7 * 24 * 60 * 60 * 1000
-  });
+    res.cookie("token", token, {
+        httpOnly: true,
+        secure: true,            
+        sameSite: "None",         
+        maxAge: 7 * 24 * 60 * 60 * 1000, // 7 din
+    });
 }
 
 router.post('/register', validate(registerSchema), async (req, res, next) => {
-  try {
-    const { email, password } = req.body;
-    const { user, token } = await register(email, password);
-    setAuthCookie(res, token);
-    res.status(201).json({ user: { id: user._id, email: user.email } });
-  } catch (e) { next(e); }
+    try {
+        const { email, password } = req.body;
+        const { user, token } = await register(email, password);
+        setAuthCookie(res, token);
+        res.status(201).json({ user: { id: user._id, email: user.email } });
+    } catch (e) { next(e); }
 });
 
 router.post('/login', validate(loginSchema), async (req, res, next) => {
-  try {
-    const { email, password } = req.body;
-    const { user, token } = await login(email, password);
-    setAuthCookie(res, token);
-    res.json({ user: { id: user._id, email: user.email } });
-  } catch (e) { next(e); }
+    try {
+        const { email, password } = req.body;
+        const { user, token } = await login(email, password);
+        setAuthCookie(res, token);
+        res.json({ user: { id: user._id, email: user.email } });
+    } catch (e) { next(e); }
 });
 
 router.post('/logout', (req, res) => {
-  res.clearCookie('token');
-  res.status(204).end();
+    res.clearCookie('token');
+    res.status(204).end();
 });
 
 router.get('/me', (req, res) => {
-  const t = req.cookies?.token;
-  if (!t) return res.status(200).json({ user: null });
-  res.status(200).json({ user: true });
+    const t = req.cookies?.token;
+    if (!t) return res.status(200).json({ user: null });
+    res.status(200).json({ user: true });
 });
 
 export default router;
